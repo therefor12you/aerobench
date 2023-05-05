f0f678d97bfa9dcaf3e88af2ff69530cf89ef8a5
therefor12you <lzpkzd123@gmail.com>
2023-05-05 17:10:01 +0800

    机载雷达模块已修改，还未test

--- AI_F16/Code/Simulation/Conponents/Radar.py
+++ AI_F16/Code/Simulation/Conponents/Radar.py
@@ -1,10 +1,12 @@
 import numpy as np
 from random import random
+import rticonnextdds_connector as rti
+from os import path 
 from math import cos, sin, atan, sqrt, radians
 from vpython import *
 
-# 目前均在雷达坐标系下完成扫描, 需要转换至地面坐标系, 抓紧时间完成
-# 当前实现的是单目标跟踪扫描
+# 目前均在雷达坐标系下完成扫描, 需要转换至地面坐标系 
+# 当前实现的是单目标跟踪扫描, 无法实现多目标跟踪
 
 class Radar:
     def __init__(self):
@@ -23,7 +25,6 @@ class Radar:
         # 雷达的扫描模式，0为正常扫描，1为锁定
         self.ScanMode = 0
 
-        
         # 雷达方位扫描范围, 根据搜索模式[10, 30, 60, 120]选其一[°]
         self.AziRange = radians(60)
         # 雷达俯仰扫描范围, 根据搜索模式[4.5, 60]选其一[°]
@@ -54,20 +55,49 @@ class Radar:
         # 雷达探测到的目标信息
         self.TarInfo = None
 
+        # DDS
+        self.file_path = path.dirname(path.realpath(__file__))
+        self.pub_connector = rti.Connector("MyParticipantLibrary::MyPubParticipant", self.file_path + "\ShapeExample.xml")
+        self.sub_connector = rti.Connector("MyParticipantLibrary::MySubParticipant", self.file_path + "\ShapeExample.xml")
+
+        self.output = self.pub_connector.get_output("MyPublisher::MyRadarWriter")
+        self.output.instance.set_dictionary({"scan_mode":self.ScanMode, "search_mode":self.SearchMode, "target_position":self.TarInfo})
+        self.output.write()
+
 
     # 计算目标与本机距离信息
-    def distance(self, my_aircraft, target):
+    def distance(self, my_aircraft_position, target_position):
         # 地面坐标系下的双机距离信息
-        Rx = float(target.position[0] - my_aircraft.position[0])
-        Ry = float(target.position[1] - my_aircraft.position[1])
-        Rz = float(target.position[2] - my_aircraft.position[2])
+        Rx = float(target_position[0] - my_aircraft_position[0])
+        Ry = float(target_position[1] - my_aircraft_position[1])
+        Rz = float(target_position[2] - my_aircraft_position[2])
         dist = sqrt(Rx**2 + Ry**2 + Rz**2)
         return [dist, Rx, Ry, Rz]
 
-    # 设定搜索模式
-    def mode(self, my_aircraft, target):
-        dist = self.distance(my_aircraft, target)
+  
+    # 扫描主函数
+    def scan(self, t):
+        # 订阅载机信息
+        input = self.sub_connector.get_input("MySubscriber::MyAircraftReader")
+        input.take()
+        for sample in input.samples.valid_data_iter:
+            # You can get all the fields in a get_dictionary()
+            data = sample.get_dictionary()
+            base_position = [data['state'][i] for i in [10,9,11]]
+            base_velocity = data['state'][0]
+            # print('得到飞机坐标{}, 速度{}'.format(base_position, base_velocity))
+        # 订阅导弹信息
+        input = self.sub_connector.get_input("MySubscriber::MyMissileReader")
+        input.take()
+        for sample in input.samples.valid_data_iter:
+            # You can get all the fields in a get_dictionary()
+            data = sample.get_dictionary()
+            tgt_position = data['position']
+            tgt_velocity = data['velocity']
 
+        dist = self.distance(base_position, tgt_position)
+
+        # 搜索模式
         if dist[0] > 15000:
             # 超视距
             self.SearchMode = 1
@@ -78,12 +108,8 @@ class Radar:
             self.SearchMode = 2
             self.AziRange = radians(10)
             self.EleRange = radians(60)
-    
 
-    # 计算目标与本机的视线角，并判断是否截获目标
-    def SightAngle(self, my_aircraft, target):
-        dist = self.distance(my_aircraft, target)
-
+        # 计算视线角
         q_z = atan(sqrt(dist[1]**2 + dist[2]**2)/dist[3]) if dist[3]!=0 else 0
         q_x = atan(dist[2]/dist[1]) if dist[1]!=0 else np.pi/2
         
@@ -95,14 +121,11 @@ class Radar:
                 self.ScanMode = 1       # 锁定目标
                 # 雷达波束持续照射目标
                 # self.AxisPos = [dist[0]*sin(q_z)*cos(q_x), dist[0]*sin(q_z)*sin(q_x), dist[0]*cos(q_z)]
-                self.AxisPos = target.position
-                self.TarInfo = target
+                self.AxisPos = tgt_position
+                self.TarInfo = tgt_position
         else:
             self.ScanMode = 0       
 
-        
-    # 扫描
-    def scan(self, t):
         # 如果雷达处于扫描状态，根据时间步长更新雷达方位角和俯仰角，以及波束中心轴坐标
         if self.ScanMode == 0:
             tsl = t%self.T 
@@ -135,7 +158,11 @@ class Radar:
                             # 此时雷达从右至左扫描
                             self.Azi -= self.Vscan*t_in
             # 更新波束轴坐标
-            self.AxisPos = [self.max_detected_distance*cos(self.Ele)*sin(self.Azi), self.max_detected_distance*cos(self.Ele)*cos(self.Azi), self.max_detected_distance*sin(self.Ele)]
+            self.AxisPos = [base_position[0] + self.max_detected_distance*cos(self.Ele)*sin(self.Azi), base_position[1] + self.max_detected_distance*cos(self.Ele)*cos(self.Azi), base_position[2] + self.max_detected_distance*sin(self.Ele)]
+        
+        # 发布雷达信息
+        self.output.instance.set_dictionary({"scan_mode":self.ScanMode, "search_mode":self.SearchMode, "target_position":self.TarInfo})
+        self.output.write()
 
 class UAV:
     def __init__(self, position, speed):
